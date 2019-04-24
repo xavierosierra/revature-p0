f959953a22c64b62db5896e2997a012105aeb60e
Xavthaman <44090635+Xavthaman@users.noreply.github.com>
2019-04-23 19:24:43 -0500

    node-web

--- node-web.sh
+++ node-web.sh
@@ -3,25 +3,72 @@
 #include: node, 1 script with 2 functions
 #name: node-web.sh
 
+#check to see if node is currently installed
+if [ -z "$(which node)" ]; then
+   echo "you currently dont have node installed"
+   exit 1
+fi
 
-##automate 
-
-#create start function for node 
-startNode()
+## Start Function ##
+start()
 {   
-    startscript=$(which package.json | grep -E "start")
-    npm run-script prueba.sh
+    # the directory you are trying to access
+    directory_name=$1
+
+    # checks to see if the directory exist
+    if ! [ -d $directory_node ]; then
+        echo "this is not a directory"
+        exit 1
+    fi
+
+    # Goes to the directory you write at the command.
+    cd $directory_name
+    
+    # Grep the inside of the package.json and then searches for the word start
+    result=$(cat package.json | grep -E 'start')
+
+    # checks to see if start was found, if it was it will run npm start
+    if [ -z "$result" ]; then 
+        echo "there is no start script in the package.json"
+        exit 1
+    fi
+
+    # runs the start script in the package.json
     npm start
-       echo "NPM on"
 }
- 
- #Create stop function for node 
-stopNode()
+
+stop()
 {
-     stopscript=$(which package.json | grep -E "stop")
+
+    
+    directory_name=$1
+
+   
+    if ! [ -d $directory_name ]; then
+        echo "this is not a directory"
+        exit 1
+    fi
+
+ 
+    cd $directory_name
+    
+    
+    result=$(cat package.json | grep -E 'stop')
+
+   
+    if [ -z "$result" ]; then 
+        echo "there is no stop script in the package.json"
+        exit 1
+    fi
+
+   
     npm stop
-    echo "NPM stop"
 }
+
+# the command you want to call in the script, "start" or "stop"
+command=$1
+
+# the case checks to see if the command was called properly, if a call is made that isnt either "start" or "stop" it will echo an error
 case $command in
     "start")
         start $2
--- prueba.sh
+++ prueba.sh
@@ -1,38 +1,84 @@
-assignRole()
- {   #the command to assing
-      rolechange=$1
-      username=$2
-      userrole=$3
-      if [ -z $rolechange ]; then 
-         echo 'Change the users role' 
-        exit 1
+#!/bin/bash
 
-       elif [ -z $username ]; then 
-         echo 'Change the user'
-        exit 1
 
-       elif [ -z $userrole ]; then 
-         echo ''
-        exit 1
-        fi
-
-      
-     az role assignment $action \
-      --assignee $username \
-      --role $role
- }
- admin=$(az role assignment list \
-    --include-classic-administrators \
-    --query "[?id=='NA(classic admins)'].principalName" | grep -E $adminUserName)
-
-# checks to see if the user is an admin
-if [ -z "$admin" ]; then 
-    echo "you must be an admin to access this file"
-    exit 1
+#check to see if node is currently installed
+if [ -z "$(which node)" ]; then
+   echo "you currently dont have node installed"
+   exit 1
 fi
 
-# where you call the functions create, assign and delete
-command=$2
-$command $3 $4 $5
+## Start Function ##
+start()
+{   
+    # the directory you are trying to access
+    directory_name=$1
+
+    # checks to see if the directory exist
+    if ! [ -d $directory_node ]; then
+        echo "this is not a directory"
+        exit 1
+    fi
+
+    # Goes to the directory you write at the command.
+    cd $directory_name
+    
+    # Grep the inside of the package.json and then searches for the word start
+    result=$(cat package.json | grep -E 'start')
+
+    # checks to see if start was found, if it was it will run npm start
+    if [ -z "$result" ]; then 
+        echo "there is no start script in the package.json"
+        exit 1
+    fi
+
+    # runs the start script in the package.json
+    npm start
+}
+
+stop()
+{
+
+    
+    directory_name=$1
+
+   
+    if ! [ -d $directory_name ]; then
+        echo "this is not a directory"
+        exit 1
+    fi
+
+ 
+    cd $directory_name
+    
+    
+    result=$(cat package.json | grep -E 'stop')
+
+   
+    if [ -z "$result" ]; then 
+        echo "there is no stop script in the package.json"
+        exit 1
+    fi
+
+   
+    npm stop
+}
+
+# the command you want to call in the script, "start" or "stop"
+command=$1
+
+# the case checks to see if the command was called properly, if a call is made that isnt either "start" or "stop" it will echo an error
+case $command in
+    "start")
+        start $2
+    ;;
+    "stop")
+        stop $2
+    ;;
+    *)
+        echo "must use either start or stop as your command"
+        exit 1
+    ;;
+
+esac
 
 exit 0
